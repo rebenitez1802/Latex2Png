2fe6f887988d3aa36ce73f9ce15b9986282532ea
Rafael Benitez <rebenitez1802@gmail.com>
2018-05-03 13:36:13 -0300

    add test method

--- index.js
+++ index.js
@@ -13,10 +13,6 @@ mjAPI.config({
   }
 });
 mjAPI.start();
- 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
 
 app.post('/', function (req, res) {
 	var promises = [];console.log(req.body);
@@ -44,7 +40,11 @@ app.post('/', function (req, res) {
 	
 	
 });
-
+app.get('/', function (req, res) {
+	res.send(JSON.stringify({test:'OK'}))
+	
+	
+});
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
