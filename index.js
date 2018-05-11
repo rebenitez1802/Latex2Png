d1b4e98d4716036f2c4082081c71835e6a2226c2
Rafael Benitez <rebenitez1802@gmail.com>
2018-05-11 15:37:04 -0300

    added support to recive csstext on body

--- index.js
+++ index.js
@@ -15,10 +15,40 @@ mjAPI.config({
 mjAPI.start();
 
 app.post('/', function (req, res) {
+	
 	var promises = [];console.log(req.body);
 	for(var item in req.body.list){
-		console.log('adding '+req.body.list[item].math+ ' '+item);
-		promises.push(mjAPI.typeset({png:true, math:req.body.list[item].math}));
+		var scale = 1;
+		var stylesObject=null;
+		if(req.body.list[item].cssText!='' || req.body.list[item].cssText!=undefined){
+			var style = req.body.list[item].cssText;
+			var font_weight = new RegExp(/font-weight: (\w+);/);
+			var font_size = new RegExp(/font-size: ([\w\.]+);/);
+			var color = new RegExp(/color: (\w+);/);
+			stylesObject= {};
+			stylesObject[".MathJax .merror"]={};
+			if(font_weight.exec(style))
+				stylesObject[".MathJax .merror"]["font-weight"]= font_weight.exec(style)[1];
+			if(font_size.exec(style)){
+				var cssFontSizeString = font_size.exec(style)[1];
+				var patt2 = new RegExp(/(\d+\.\d+)px;/);
+				var actualSize = parseFloat(patt2.exec(style)[1]);
+				stylesObject[".MathJax .merror"]["font-size"]= font_size.exec(style)[1];
+				scale=actualSize/17;
+			}
+			if(color.exec(style))
+				stylesObject[".MathJax .merror"]["color"]= color.exec(style)[1];
+			if(req.body.list[item].scale!= '' && req.body.list[item].scale!=undefined)
+				scale=req.body.list[item].scale
+
+		}
+		console.log(stylesObject);
+		mjAPI.config({
+			MathJax:{
+				style:stylesObject
+			}
+		})
+		promises.push(mjAPI.typeset({scale:scale ,png:true, math:req.body.list[item].math}));
 	}
 	Promise.all(promises).then(function(values){
 		var ress = {pngs:[]};
@@ -33,6 +63,8 @@ app.post('/', function (req, res) {
 			
 		}
 		res.setHeader('Access-Control-Allow-Origin', '*');
+		res.setHeader('Access-Control-Allow-Methods', '*');
+		res.setHeader('Access-Control-Allow-Headers', '*');
 	    res.setHeader('Content-Type', 'application/json');
   		
   		res.send(JSON.stringify(ress));
